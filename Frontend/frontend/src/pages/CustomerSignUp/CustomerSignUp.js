87cd4773b62457d261c54850b0ff0cc3ba214131
abelkmathew <abelkmathew@gmail.com>
2021-11-24 12:15:12 +0530

    Customer SignUp

--- Frontend/frontend/src/pages/CustomerSignUp/CustomerSignUp.js
+++ Frontend/frontend/src/pages/CustomerSignUp/CustomerSignUp.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 function CustomerSignUp() {
     return (
-        <div>
-            CustomerSignUp
+        <div className="customerSignup">
+            <div className="container form">
+                    <div className="headText">CustomerSignUp</div>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="First Name"></input>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Last Name"></input>
+                    <input type="text" class="form-control em" id="exampleFormControlInput1" placeholder="Location"></input>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email"></input>
+                    <div className="pwd"><input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password"></input>
+                    </div>
+                    
+                    <button type="button" class="btn ">Sign Up</button>
+                </div>
         </div>
     )
 }
