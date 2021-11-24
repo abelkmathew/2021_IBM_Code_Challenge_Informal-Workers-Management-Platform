30a9a23c072d67cf99659ba9fb8c9160b03489a0
abelkmathew <abelkmathew@gmail.com>
2021-11-24 08:35:32 +0530

    login, aboutus page

--- Frontend/frontend/src/pages/AboutUs/AboutUs.js
+++ Frontend/frontend/src/pages/AboutUs/AboutUs.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import './aboutus.css';
+import image from './about.png'
+
 
 function AboutUs() {
     return (
-        <div>
-            AboutUs
+        <div className="AboutPage">
+            <div className="container">
+                <div className="aboutHeading"><h1>About Us</h1>
+                <h3>Our Vision</h3></div>
+                <div className="col-md-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Aenean maximus mauris nec sapien accumsan, vel dapibus ex auctor. 
+                    Donec suscipit lorem nulla, sed consequat arcu eleifend euismod. 
+                    Pellentesque mattis nisl non enim dapibus, eu congue urna fermentum. 
+                    Proin ut justo blandit, scelerisque justo sed, accumsan purus. 
+                    Etiam suscipit vehicula ullamcorper. Nunc efficitur, dui vel hendrerit 
+                    sollicitudin, magna erat faucibus augue, lobortis lobortis lacus massa 
+                    vel mauris. Sed tortor elit, ultrices non sollicitudin ac, lobortis vitae 
+                    dolor. Phasellus placerat maximus odio eu tincidunt. Maecenas ut 
+                    egestas turpis. Aliquam malesuada sagittis eros, eu fringilla nunc 
+                    rutrum a. Phasellus non iaculis metus. Aenean ut libero eu lacus fermentum 
+                    accumsan non quis nibh.</p></div>
+                    <div className="col-md-6">
+                  <img className="img-fluid" src={image} alt="Vision"></img>
+                </div>
+            </div>
         </div>
     )
 }
--- Frontend/frontend/src/pages/Login/Login.js
+++ Frontend/frontend/src/pages/Login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './login.css';
+import { Link } from 'react-router-dom';
 
 function Login() {
     return (
-        <div>
-            Login
+        <div className="loginPage">
+            <div className="loginPageWrapper">
+                <div className="form">
+                    <div className="headText">Login Now !</div>
+                    <input type="email" class="form-control em" id="exampleFormControlInput1" placeholder="Email"></input>
+                    <div className="pwd"><input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password"></input>
+                    </div>
+                    <div className="forgotPWD"><a href="#">Forgot password ?</a></div>
+                    <button type="button" class="btn ">Login</button>
+                    <div className="signUp">
+                        Don't have an account?
+                        <Link to="signUp"> Sign Up</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
