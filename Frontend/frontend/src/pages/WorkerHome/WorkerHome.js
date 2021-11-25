dc97ceb427cbaaf0eb916f4d22582141c175b7e6
abelkmathew <abelkmathew@gmail.com>
2021-11-25 09:56:51 +0530

    Worker/Customer Modifications

--- Frontend/frontend/src/pages/WorkerHome/WorkerHome.js
+++ Frontend/frontend/src/pages/WorkerHome/WorkerHome.js
@@ -5,6 +5,9 @@ import Post from '../../components/Post/Post'
 function WorkerHome() {
     return (
         <div className="workerhome">
+            <div className="HeadText">
+                Job Lists
+            </div>
             <Post />
             <Post />
         </div>
